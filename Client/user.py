652726e2bc9d316a3bce80e1fb62a8664e3f78b7
Emanuel Antonio Lima Pereira <emanuelantoniolimapereira@gmail.com>
2024-05-24 16:05:39 -0300

    feat commit (class client)

---
+++ Client/user.py
@@ -0,0 +1,11 @@
+from client import *
+
+def main():
+    
+    while True:
+        print(" 1 - Entrar\n 2 - Criar conta\n ")
+        opcao = int(input("===>"))
+        
+if __name__=="__main__":
+    main()
+    
