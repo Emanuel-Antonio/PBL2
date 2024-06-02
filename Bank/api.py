76814f5e9fa5947c02cd4d1c299a6c33400a7213
Emanuel Antonio Lima Pereira <emanuelantoniolimapereira@gmail.com>
2024-06-02 11:27:47 -0300

    feat lock api

--- Bank/api.py
+++ Bank/api.py
@@ -2,16 +2,18 @@ from flask import Flask, request, jsonify
 import requests
 import json
 import os
+from threading import Lock
 
 app = Flask(__name__)
 
-#bank = "172.16.103.14"
-
-#bank = os.getenv("bank")
+# bank = "172.16.103.14"
+# bank = os.getenv("bank")
 bank = "192.168.1.105"
 
 # Lista de usuários
 users = []
+# Lock para sincronização
+lock = Lock()
 
 # Rota para listar todos os usuários
 @app.route('/users', methods=['GET'])
@@ -21,37 +23,43 @@ def get_users():
 @app.route('/deposito', methods=['POST'])
 def get_deposito():
     data = request.get_json()
-    for item in users:
-        if item['id'] == data['destino']:
-            item['saldo'] = item['saldo'] + data['valor']
     if not data:
         return jsonify({"message": "No JSON data provided"}), 400
+
+    with lock:
+        for item in users:
+            if item['id'] == data['destino']:
+                item['saldo'] += data['valor']
     return '', 204
 
 @app.route('/saque', methods=['POST'])
 def get_saque():
     data = request.get_json()
-    for item in users:
-        if item['id'] == data['destino']:
-            if item['saldo'] < data['valor']:
-                return jsonify({"message": "Saldo insuficiente para realizar essa operacao"}), 404
-            item['saldo'] = item['saldo'] - data['valor']
     if not data:
         return jsonify({"message": "No JSON data provided"}), 400
+
+    with lock:
+        for item in users:
+            if item['id'] == data['destino']:
+                if item['saldo'] < data['valor']:
+                    return jsonify({"message": "Saldo insuficiente para realizar essa operacao"}), 404
+                item['saldo'] -= data['valor']
     return '', 204
 
 @app.route('/transferencia', methods=['POST'])
 def get_transferencia():
     data = request.get_json()
-    url_publish = f'http://{bank[:10] + str(data['destino'])[:3]}:8088/receber'
+    if not data:
+        return jsonify({"message": "No JSON data provided"}), 400
 
-    #Arrumar depois!!!!!!!!!!
+    url_publish = f'http://{bank[:10] + str(data["destino"])[:3]}:8088/receber'
+    
+    with lock:
+        for item in users:
+            if item['id'] == data['origem']:
+                if data['valor'] > item['saldo']:
+                    return jsonify({"message": "Saldo insuficiente para realizar a operacao"}), 404
 
-    #for itens in users:
-        #if itens['id'] == data['origem']:
-            #if data['valor'] > itens['saldo']:
-                #return jsonify({"message": "Saldo insuficiente para realizar a operacao"}), 404
-            
     try:
         payload = {'destino': data['destino'], 'valor': data['valor'], 'tipo': 'transferencia', 'origem': data['origem']}  
         json_payload = json.dumps(payload)  
@@ -60,31 +68,31 @@ def get_transferencia():
         response_publish = requests.post(url_publish, data=json_payload, timeout=2, headers=headers)
 
         if response_publish.status_code == 204:
-            for itens in users:
-                if itens['id'] == data['origem']:
-                    itens['saldo'] = itens['saldo'] - data['valor']
+            with lock:
+                for item in users:
+                    if item['id'] == data['origem']:
+                        item['saldo'] -= data['valor']
             return '', 204
         else:
             response_json = response_publish.json()
             error_message = response_json.get('error', 'Unknown error')
-            #print("Erro ao enviar os dados UDP para a API:", error_message)
-            return jsonify({"message": "{}".format(error_message)}), 404 #mexer
+            return jsonify({"message": "{}".format(error_message)}), 404
     except Exception as e:
         print(f'Não foi possível estabelecer uma conexão com o Broker ... {e}')
+        return jsonify({"message": "Erro na conexão com o Broker"}), 500
 
 @app.route('/receber', methods=['POST'])
 def get_receber():
     data = request.get_json()
-    i = 0
-    for item in users:
-        if item['id'] == data['destino']:
-            item['saldo'] = item['saldo'] + data['valor']
-            i = 1
-    if i == 0:
-        return jsonify({"message": "Conta inexistente"}), 404
     if not data:
         return jsonify({"message": "No JSON data provided"}), 400
-    return '', 204
+
+    with lock:
+        for item in users:
+            if item['id'] == data['destino']:
+                item['saldo'] += data['valor']
+                return '', 204
+    return jsonify({"message": "Conta inexistente"}), 404
 
 # Rota para obter um usuário por ID
 @app.route('/users/<int:user_id>', methods=['GET'])
@@ -98,7 +106,8 @@ def get_user(user_id):
 @app.route('/users', methods=['POST'])
 def createUser():
     newUser = request.json
-    users.append(newUser)
+    with lock:
+        users.append(newUser)
     return jsonify(newUser), 201
 
 # Rota para atualizar um usuário existente
@@ -107,15 +116,18 @@ def updateUser(user_id):
     user = next((user for user in users if user['id'] == user_id), None)
     if not user:
         return jsonify({'message': 'User não encontrado'}), 404
+
     dataUpdate = request.json
-    user.update(dataUpdate)
+    with lock:
+        user.update(dataUpdate)
     return jsonify(user)
 
 # Rota para excluir um usuário
 @app.route('/user/<int:user_id>', methods=['DELETE'])
 def deleteUser(user_id):
     global users
-    users = [user for user in users if user['id'] != user_id]
+    with lock:
+        users = [user for user in users if user['id'] != user_id]
     return jsonify({'message': 'User excluído com sucesso'})
 
 if __name__ == "__main__":
