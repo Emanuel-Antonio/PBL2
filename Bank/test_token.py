69fd4612710a108b403409e99adadcf13bbf678f
Emanuel Antonio Lima Pereira <emanuelantoniolimapereira@gmail.com>
2024-06-21 15:26:03 -0300

    feat update test_token.py

--- Bank/test_token.py
+++ Bank/test_token.py
@@ -13,7 +13,8 @@ node_state = {
     "has_token": False,
     "last_token_time": time.time(),
     "token_timeout": 30,  # Tempo máximo que o nó pode ficar sem token (em segundos)
-    "token_check_interval": 1  # Intervalo de checagem (em segundos)
+    "token_check_interval": 1,  # Intervalo de checagem (em segundos)
+    "token_sequence": 0
 }
 
 def pass_token():
@@ -21,11 +22,11 @@ def pass_token():
     initial_index = node_state["current_index"]
     while True:  
         if node_state["has_token"]:
-            next_node_url = node_state["node_urls"][node_state["current_index"]]
-            print(f"Tentando passar o token para: {next_node_url}")
+            next_node_url = (node_state["current_index"] + 1) % len(node_state["node_urls"])
+            print(f"Tentando passar o token para: {node_state["node_urls"][next_node_url]}")
             time.sleep(2)      
             try:
-                response = requests.post(f"{next_node_url}/receive_token", timeout=5)
+                response = requests.post(f"{node_state["node_urls"][next_node_url]}/receive_token",json={"Token sequence": node_state['token_sequence']+1}, timeout=5)
                 if response.status_code == 200:
                     print(f"Token passado para o próximo nó: {next_node_url}")
                     node_state["last_token_time"] = time.time()
@@ -38,7 +39,7 @@ def pass_token():
             
             # Tentar o próximo nó na lista
             print((node_state["current_index"] + 1) % len(node_state["node_urls"]))
-            node_state["current_index"] = (node_state["current_index"] + 1) % len(node_state["node_urls"])
+            #node_state["current_index"] = (node_state["current_index"] + 1) % len(node_state["node_urls"])
             
             # Se voltar ao nó inicial, significa que todos os nós foram tentados
             if node_state["current_index"] == initial_index:
@@ -54,6 +55,8 @@ def receive_token():
     node_state["has_token"] = True
     node_state["last_token_time"] = time.time()
     print(f"Token recebido pelo nó {node_state['node_id']}")
+    print(f"Token recebido com sequencia {node_state['token_sequence']}")
+
 
 def check_token():
     while True:
@@ -65,7 +68,11 @@ def check_token():
 # Rota para receber o token
 @app.route('/receive_token', methods=['POST'])
 def receive_token_route():
-    receive_token()
+    global node_state
+    token_sequence = request.get_json()
+    if token_sequence["Token sequence"] > node_state["token_sequence"]:
+        node_state["token_sequence"] = token_sequence['Token sequence']
+        receive_token()
     return jsonify({"message": "Token recebido"}), 200
 
 # Função para iniciar o servidor Flask
